14485d69fd41030136c36979d355e26d9ae77e91
Lars Erik Wik <lars.erik.wik@northern.tech>
2024-12-23 12:27:23 +0100

    Player inherits GameObject
    Ticket: None
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- src/player.c
+++ src/player.c
@@ -13,8 +13,13 @@
   "assets/FREE Mana Seed Character Base "                                      \
   "Demo/char_a_p1/char_a_p1_0bas_humn_v01.png"
 
-static void OnUpdate(GameObject *player, SDL_Renderer *renderer) {
-  assert(player != NULL);
+typedef struct {
+  struct GameObject super;
+} Player;
+
+static void OnUpdate(GameObject *game_object, SDL_Renderer *renderer) {
+  assert(game_object != NULL);
+  Player *player = (Player *)game_object;
 
   const bool *keyboard_state = SDL_GetKeyboardState(NULL);
   assert(keyboard_state != NULL);
@@ -25,83 +30,90 @@ static void OnUpdate(GameObject *player, SDL_Renderer *renderer) {
     return;
   }
 
-  if (keyboard_state[SDL_SCANCODE_W] && player->position.y > 0.0f) {
-    player->position.y = MAX(player->position.y - player->velocity, 0.0f);
+  if (keyboard_state[SDL_SCANCODE_W] && player->super.position.y > 0.0f) {
+    player->super.position.y =
+        MAX(player->super.position.y - player->super.velocity, 0.0f);
   }
 
-  if (keyboard_state[SDL_SCANCODE_A] && player->position.x > 0.0f) {
-    player->position.x = MAX(player->position.x - player->velocity, 0.0f);
+  if (keyboard_state[SDL_SCANCODE_A] && player->super.position.x > 0.0f) {
+    player->super.position.x =
+        MAX(player->super.position.x - player->super.velocity, 0.0f);
   }
 
   if (keyboard_state[SDL_SCANCODE_S] &&
-      player->position.y < (height - player->size.height)) {
-    player->position.y = MIN(player->position.y + player->velocity, height);
+      player->super.position.y < (height - player->super.size.height)) {
+    player->super.position.y =
+        MIN(player->super.position.y + player->super.velocity, height);
   }
 
   if (keyboard_state[SDL_SCANCODE_D] &&
-      player->position.x < (width - player->size.width)) {
-    player->position.x = MIN(player->position.x + player->velocity, width);
+      player->super.position.x < (width - player->super.size.width)) {
+    player->super.position.x =
+        MIN(player->super.position.x + player->super.velocity, width);
   }
 }
 
-static void OnDraw(GameObject *player, TextureMap *texture_map,
+static void OnDraw(GameObject *game_object, TextureMap *texture_map,
                    SDL_Renderer *renderer) {
-  assert(player != NULL);
+  assert(game_object != NULL);
   assert(renderer != NULL);
+
+  Player *player = (Player *)game_object;
 
   if (!SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255)) {
     LOG_ERROR("Failed to set draw color: %s", SDL_GetError());
   }
 
   SDL_FRect rect = {
-      .x = player->position.x,
-      .y = player->position.y,
-      .w = player->size.width,
-      .h = player->size.height,
+      .x = player->super.position.x,
+      .y = player->super.position.y,
+      .w = player->super.size.width,
+      .h = player->super.size.height,
   };
 
   if (!SDL_RenderRect(renderer, &rect)) {
     LOG_ERROR("Failed to draw rectangle: %s", SDL_GetError());
   }
 
-  TextureMapDrawFrame(texture_map, player->texture_id, renderer,
-                      player->position.x, player->position.y,
-                      player->size.width, player->size.height, 0, 0, 0.0, 255,
-                      SDL_FLIP_NONE);
+  TextureMapDrawFrame(texture_map, TEXTURE_ID, renderer,
+                      player->super.position.x, player->super.position.y,
+                      player->super.size.width, player->super.size.height, 0, 0,
+                      0.0, 255, SDL_FLIP_NONE);
 }
 
-static void OnClean(GameObject *player, TextureMap *texture_map) {
-  assert(player != NULL);
+static void OnClean(GameObject *game_object, TextureMap *texture_map) {
+  assert(game_object != NULL);
+  assert(texture_map != NULL);
 
-  if (player->texture_id != NULL) {
+  Player *player = (Player *)game_object;
+
+  if (player->super.texture_id != NULL) {
     LOG_DEBUG("Destroying texture");
-    TextureMapClearTexture(texture_map, player->texture_id);
+    TextureMapClearTexture(texture_map, TEXTURE_ID);
   }
 
   free(player);
 }
 
 GameObject *PlayerCreate(TextureMap *texture_map, SDL_Renderer *renderer) {
-  GameObject *player = xmalloc(sizeof(GameObject));
+  Player *player = xmalloc(sizeof(Player));
 
-  player->position.x = 0.0f;
-  player->position.y = 0.0f;
+  player->super.position.x = 0.0f;
+  player->super.position.y = 0.0f;
 
-  player->size.width = 64.0f;
-  player->size.height = 64.0f;
+  player->super.size.width = 64.0f;
+  player->super.size.height = 64.0f;
 
-  player->velocity = 3.0f;
+  player->super.velocity = 3.0f;
 
-  player->callback.update = OnUpdate;
-  player->callback.draw = OnDraw;
-  player->callback.clean = OnClean;
+  player->super.callback.update = OnUpdate;
+  player->super.callback.draw = OnDraw;
+  player->super.callback.clean = OnClean;
 
-  player->texture_id = TEXTURE_ID;
-
-  if (!TextureMapLoadTexture(texture_map, TEXTURE_FILENAME, player->texture_id,
+  if (!TextureMapLoadTexture(texture_map, TEXTURE_FILENAME, TEXTURE_ID,
                              renderer)) {
-    LOG_ERROR("Failed to load texture '%s'", player->texture_id);
+    LOG_ERROR("Failed to load texture '%s'", TEXTURE_ID);
   }
 
-  return player;
+  return (GameObject *)player;
 }
